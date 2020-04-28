18572e67f59c44d0589cd91ef670035b75112dd8
Dante L'Erario <dantelerario@gmail.com>
2020-04-28 15:43:37 +0200

    JS working

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
-	//Code
+	var cdList = $('.cds-container')
+  var cdArray = [];
+  // handlebars
+  var source = $('#template').html();
+  var template = Handlebars.compile(source);
+  // loading cds in html
+  for (i = 0; i < 10; i++) {
+    $.ajax({
+      url: 'https://flynn.boolean.careers/exercises/api/array/music',
+      method: 'GET',
+      success: function(data) {
+        cdArray.push
+
+
+      },
+      error: function() {
+        console.log('ERROR');
+      },
+    })
+  }
+
 });
